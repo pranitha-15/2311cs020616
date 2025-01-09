4997bd100c29b2bb513a2b2103bd07007ca3c48c
pranitha-15 <pranithaa1510@gmail.com>
2025-01-09 21:42:43 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,20 @@
+# Declare variables of different data types
+
+# Integer
+integer_var = 10
+
+# Float
+float_var = 3.14
+
+# String
+string_var = "Hello, Jupyter!"
+
+# Boolean
+boolean_var = True
+
+# Print each variable and its type
+
+print("Integer Variable: ", integer_var, " Type: ", type(integer_var))
+print("Float Variable: ", float_var, " Type: ", type(float_var))
+print("String Variable: ", string_var, " Type: ", type(string_var))
+print("Boolean Variable: ", boolean_var, " Type: ", type(boolean_var))
