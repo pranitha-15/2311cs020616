8e96163336a2f29bdeb2b4974b5f759c15f13c7d
pranitha-15 <pranithaa1510@gmail.com>
2025-01-10 11:59:25 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,8 @@
+# Creating a List with 5 elements
+my_list = [10, 20, 30, 40, 50]
+
+# Accessing elements by index
+print(my_list[0])   # Access the first element (index 0)
+print(my_list[2])   # Access the third element (index 2)
+print(my_list[-1])  # Access the last element (negative index -1)
+print(my_list[1:4]) # Access a range of elements (index 1 to 3)
